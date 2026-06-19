d4265a38829e315bde208c1fbf11b3b5343ebd84
JahnaviGowda-v <jahnaviigowda@gmail.com>
2026-06-19 10:34:58 +0530

    Added advanced_calculator.py

---
+++ advanced_calculator.py
@@ -0,0 +1,15 @@
+import math
+
+num = float(input("Enter a number: "))
+
+print("1. Square Root")
+print("2. Square")
+
+choice = input("Choose (1/2): ")
+
+if choice == "1":
+    print("Result:", math.sqrt(num))
+elif choice == "2":
+    print("Result:", num ** 2)
+else:
+    print("Invalid choice")
